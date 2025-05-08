947adac639b73a27b217f84fb7c094627cc0430b
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-05-08 17:43:22 +0900

    vercelデプロイエラー対応（prisma）

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['generated/prisma/**/*']
+    ignores: ['app/generated/prisma/**/*']
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
