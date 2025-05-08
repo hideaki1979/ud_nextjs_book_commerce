e9ca05e5344bf7eb042f46029bd7082d2bfd4775
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-05-08 18:45:19 +0900

    vercelデプロイエラー対応（prisma）

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -12,8 +12,7 @@ if (!globalForPrisma.prisma) {
 
 const prisma = globalForPrisma.prisma || new PrismaClient()
 
-export default prisma
-
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
 }
+export default prisma
