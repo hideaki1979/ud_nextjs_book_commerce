761fcb080139ef89cc2f93dd891516ca4528e30b
HIDEAKI_KAGAMI <syumeikyo@outlook.jp>
2025-05-11 17:17:53 +0900

    not-foundページの整形

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>404 Not Found</div>
+        <div className='flex items-center justify-center min-h-screen'>
+            <div className='bg-gray-100 p-4 rounded-md shadow'>
+                <h1 className='text-2xl font-bold mb-4'>
+                    404 Not Found
+                </h1>
+                <p className='text-xl font-semibold'>お探しのページは存在しません。</p>
+            </div>
+        </div>
     )
 }
 
